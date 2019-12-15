53880d9451c826480b7601b956dccab2ba54b8d8
Kevin <mrhallonline@gmail.com>
2019-12-15 00:58:02 -0500

    for loop for adding markers from text file

--- interactiveMap.py
+++ interactiveMap.py
@@ -1,13 +1,24 @@
 import folium
+import pandas as pd
+
+#open txt file with volcano data
+data = pd.read_csv("Volcanoes.txt")
+
+latitude_list = list(data["LAT"])
+longitude_list = list(data["LON"])
+location_list = list(data["NAME"])
+
 
 map = folium.Map(location=[18.462398, -77.326442], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[18.462398, -77.326442], [17.462398, -77.326442], [15.462398, -77.326442], [13.462398, -77.326442]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
-# fg.add_child(folium.Marker(location=[17.462398, -77.326442], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
-# fg.add_child(folium.Marker(location=[15.462398, -77.326442], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
-map.add_child(fg)
 
-map.save("Map1.html")
+for latitude, longitude, location in zip(latitude_list, longitude_list, location_list):
+    fg.add_child(folium.Marker(location=[latitude, longitude], popup="Name: " + location, icon=folium.Icon(color='green')))
+    
+# for coordinates in [(latitude_list, longitude_list)]:
+#     fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+
+map.add_child(fg)
+map.save("VolcanoMap.html")
